3f919166e704bbe0251ed0d84f58ccadf3b14689
Lucas-Borgna <lucas.borgna.16@ucl.ac.uk>
2017-11-03 23:45:01 +0000

    added comments

--- simpleMC.cc
+++ simpleMC.cc
@@ -16,10 +16,11 @@ int main(){
   TFile* file = new TFile("simpleMC.root", "recreate");
 
 // Book histograms
-
+// histograms for exercise 1
   TH1D* h_a = new TH1D("h_a", "h_a", 100,-1.0, 1.0);
   TH1D* h_b = new TH1D("h_b", "h_b", 100,-2.0,2.0);
   TH1D* h_c = new TH1D("h_c", "h_c", 100, -6.0, 6.0);
+// histograms for exercise 2
   TH1D* h_2a = new TH1D("h_2a", "h_2a", 100, 0.0, 1.0);
   TH1D* h_2b = new TH1D("h_2b","h_2b", 100, 0.0, 1.0);
 // Create a TRandom3 object to generate random numbers
@@ -36,45 +37,36 @@ int main(){
     double r2 = ran -> Rndm();
     double x1 = r1 + r2 - 1;  
     h_a -> Fill(x1);
-  }
-
-  for (int i = 0; i < numValues; i++){
-    double x2 = 0;
     double rsum = 0;
-    for(int j = 0; j < 4; j ++ ){
+    double x2 = 0;
+    for(int j = 0; j < 4; j++){
       double rj = ran -> Rndm();
       rsum = rsum + rj;
     }
-    x2 = rsum -2;
+    x2 = rsum - 2;
     h_b -> Fill(x2);
 
-  }
-
-  for (int i = 0; i < numValues; i++){
+    double rsum2 = 0;
     double x3 = 0;
-    double rsum = 0;
-    for(int j = 0; j < 12; j++){
-      double rj = ran -> Rndm();
-      rsum = rsum + rj;
+    for(int k = 0; k < 12; k++){
+      double rk = ran -> Rndm();
+      rsum2 = rsum2 + rk;
     }
-    x3 = rsum - 6;
-    h_c -> Fill(x3);    
-  }
+    x3 = rsum2 - 6;
+    h_c -> Fill(x3);
 
+  }
+//Exercise 2a and 2b 
   double xmax = 1.0;
   for (int i = 0; i < numValues; i++) {
     double r = ran -> Rndm();
     double x = sqrt(xmax*xmax - r);
     h_2a -> Fill(x); 
     double u = ran -> Rndm();
-    if (u < x) {
+    if (u < x) { //acceptance criteria
       h_2b -> Fill(x);
-
     }
   }
-
-
-
 
 // Store all histograms in the output file and close up
 
